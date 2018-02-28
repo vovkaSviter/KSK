9b52023b620afc304a5281e12b3b8584c1daed74
Vova <vovka.sviter@gmail.com>
2018-02-28 12:04:13 +0300

    First commit

---
+++ wp/functions.php
@@ -0,0 +1,8 @@
+<?php
+    function theme_enqueue_styles() {
+//        wp_enqueue_style( 'owl', get_stylesheet_directory_uri() . '/assets/css/owl.carousel.min.css' );
+//        wp_enqueue_style( 'owl', get_stylesheet_directory_uri() . '/assets/css/owl.theme.default.min.css' );
+        wp_enqueue_style( 'grid12', get_stylesheet_directory_uri() . '/assets/css/grid12.css' );
+//        wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/assets/fonts/font-awesome/css/font-awesome.min.css' );
+    }
+    add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
